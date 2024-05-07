4924ac903fcae68abf9f635b02a2211a6b36efb0
duongmarco@gmail.com <duongmarco@gmail.com>
2024-05-07 11:47:15 +0000

    update

---
+++ models/staging/raw/stg_raw__ship.sql
@@ -0,0 +1,20 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'ship') }}
+
+),
+
+renamed as (
+
+    select
+    orders_idshipping_fee,
+    log_cost,
+    ship_cost
+
+    from source
+
+)
+
+select * from renamed
